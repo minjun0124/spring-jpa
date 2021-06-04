35e9b27b950b85da910458be8c8ae3a2b30429cc
mjlee <minjun0124@gmail.com>
2021-06-04 17:07:03 +0900

    feat: search query ex

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.OrderSearch;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -23,9 +29,91 @@ public class OrderRepository {
 
     /**
      * 검색 기능
-     * 나중에 추가
      */
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//        
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+//      이렇게 하면 status, name 유무에 따라 query 내용도 달라져야하는 등
+//      동적 쿼리에 대응하기 어렵다.
+        return em.createQuery("select o from Order o join o.member m"
+                + " where o.status = :status"
+                + " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)    // (paging) 최대 1000건
+                .getResultList();
+    }
+
+
+    /**
+     * jpql을 String으로 만들어서 처리하는 방식은 복잡하며
+     * 유지보수 측면에서 어렵고 버그 발생 확률도 높다.
+     */
+    private List<Order> findAllByString(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += "o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
+    /**
+     * JPA Criteria
+     * 실무에서 쓰지 않는다.
+     * 코드만 보고 무슨 쿼리가 생성될지 예상하기가 어렵다.
+     * 유지보수가 아주 어려움.
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
 }
